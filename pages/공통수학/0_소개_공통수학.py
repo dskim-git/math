9496f7c7c6eaea38e808c5d22fb647ef0a5b65a1
dskim-git <daesobi1@gmail.com>
2025-09-17 09:20:24 +0900

    Create 0_소개_공통수학.py

---
+++ pages/공통수학/0_소개_공통수학.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from utils import set_base_page, page_header
+
+set_base_page("공통수학", "🧩")
+page_header("공통수학", "공통수학 카테고리 가이드")
+
+st.info("공통수학 시뮬레이터가 추가되면 이곳에 목록이 표시됩니다.")
