92ca4a65eb44f936401f90e230be1387b6cd7110
dskim-git <daesobi1@gmail.com>
2025-09-17 11:16:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# app.py
+# -*- coding: utf-8 -*-
 import streamlit as st
 from nav_helper import build_navigation, inject_sidebar
 
@@ -9,6 +9,6 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-inject_sidebar()           # 사이드바
-nav = build_navigation()   # 네비 생성(모든 Page 등록)
-nav.run()                  # 실행
+inject_sidebar()
+nav = build_navigation()
+nav.run()
