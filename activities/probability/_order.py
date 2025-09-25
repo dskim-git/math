a3c69cb5801d46fe0782d07c658af078619d8035
dskim-git <daesobi1@gmail.com>
2025-09-25 09:34:08 +0900

    Create _order.py

---
+++ activities/probability/_order.py
@@ -0,0 +1,20 @@
+# activities/probability/_order.py
+# .py 확장자 없이, "폴더/파일명" 형태의 slug를 나열합니다.
+ORDER = [
+    "pascal_modulo_view",
+    "buffon_needle_p5",
+    "bertrand_paradox_p5",
+    "monty_hall_p5",
+    "monty_hall_extended_p5",
+    "binomial_simulator",
+    "binomial_galton_board",
+    "binomial_normal_approx",
+    "normal_sampling"
+    "sampling_mean_demo_p5",
+    "clt_sample_mean",
+    "ci_mean_demo_p5",
+    "ci_length_lab"
+    # 하위 폴더에 있는 경우엔 이렇게 씁니다:
+    #"mini/dice_conditional_prob",      # mini 폴더 예시(보이게 하려면 해당 파일 META.hidden=False 필요)
+    # ...원하는 순서대로 계속...
+]
