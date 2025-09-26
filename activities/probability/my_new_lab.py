9a679876bf6911c37b5f4623597d14fa531f5f08
dskim-git <daesobi1@gmail.com>
2025-09-26 09:40:57 +0900

    Delete activities/probability/my_new_lab.py

--- activities/probability/my_new_lab.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-META = {
-    "title": "새 실험",
-    "description": "요약 설명",
-    # "order": 120,      # 선택: 사이드바/과목 메인 정렬에 힌트
-    # "hidden": False,   # mini/에서도 보이게 하려면 False
-}
-def render():
-    st.write("Hello, Mathlab!")
